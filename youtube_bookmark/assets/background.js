998b3ac7145b13be281683162aa00736cd48e57c
varungupta04 <87354396+varungupta04@users.noreply.github.com>
2024-11-18 22:28:25 +0530

    Update background.js

--- youtube_bookmark/assets/background.js
+++ youtube_bookmark/assets/background.js
@@ -1,11 +1,14 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//   if (tab.url && tab.url.includes("youtube.com/watch")) {
+//     const queryParameters = tab.url.split("?")[1];
+//     const urlParameters = new URLSearchParams(queryParameters);
 
-    chrome.tabs.sendMessage(tabId, {
-      type: "NEW",
-      videoId: urlParameters.get("v"),
-    });
-  }
-});
+//     chrome.tabs.sendMessage(tabId, {
+//       type: "NEW",
+//       videoId: urlParameters.get("v"),
+//     });
+//   }
+// });
+
+
+// Not needed in this folder
